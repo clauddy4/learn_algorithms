ddf917aee33a5dbbfb70ff74e7539d3c47ce97b5
Olga Makarova <53290930+clauddy4@users.noreply.github.com>
2022-09-20 09:50:55 +0300

    add 1470

---
+++ problems/1470. Shuffle the Array.js
@@ -0,0 +1,10 @@
+let shuffle = function (nums, n) {
+    let result = [];
+    for (let i = 0; i < n; i++) {
+        result.push(nums[i]);
+        result.push(nums[i + n]);
+    }
+    return result;
+};
+
+console.log(shuffle([1, 1, 2, 2], 2))
